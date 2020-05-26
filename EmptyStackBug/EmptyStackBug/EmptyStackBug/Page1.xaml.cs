873d3f376ec38442a6debec6216403c783adf306
AlexAba <alekseiabakumenko@gmail.com>
2020-05-26 22:29:12 +0300

    init

---
+++ EmptyStackBug/EmptyStackBug/EmptyStackBug/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EmptyStackBug
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            int count = Navigation.NavigationStack.Count; //    count has value 0 in this case.
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            int count = Navigation.NavigationStack.Count; //    count has value 2 in this case.
+        }
+    }
+}
